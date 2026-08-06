8be11ce0f6fc8dc1045a20a57cf4ba58ebdbd88e
Vanisha Verma <vaniiverma719@gmail.com>
2026-08-06 12:01:04 +0530

    Use deployed backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://ai-code-review-assistant-backend-wqv5.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
